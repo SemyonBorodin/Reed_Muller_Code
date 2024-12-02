ec75030aa33ec949774234757b6c73fedbaba882
Semyon Borodin <s.borodin@g.nsu.ru>
2024-12-03 01:47:49 +0700

    encoding added with many bugs

--- reed_muller_code/reed_muller_code.py
+++ reed_muller_code/reed_muller_code.py
@@ -84,6 +84,7 @@ print(encoded_message, 'encoded_message a.k.a codeword')
 print(noisy_codeword, 'noisy_codeword')
 print(rm_code.t, 'correction capability of the RM(2,4)')
 
+
 def sum_of_powers_of_two(nums, n):
     if not nums:
         return 0
@@ -92,32 +93,45 @@ def sum_of_powers_of_two(nums, n):
         total_sum += 2 ** (n - num - 1)
     return int(total_sum)
 
-def sum_of_powers_of_two_normalized(nums, n):
-    if not nums:
-        return 0
-    total_sum = 0
-    for num in nums:
-        total_sum += 2 ** (num)
-    return int(total_sum)
-# for fixed r one step
 
-def decoder_step_for_fixed_curr_r(r, m, start):
-    start = []
-    check_sum = 0
+def decoder_step_for_fixed_curr_r(message_len, word, curr_mess, r, m):
+    num_of_conjunctions = binom(m, r)
+    total_sum_for_seq_sums = 0
+    len_check_sum = 2 ** (m - r)
+    num_check_sums = 2 ** r
+    decision_sum = 0
     var_indexes = tuple(range(0, m))
-    i = 0
+    j, k = 0, 0
     for conjunction in combinations(range(m), r):
         orthogonal_subspace_positions = tuple(x for x in var_indexes if x not in conjunction)
+        if k > 0:
+            conj_number = int(message_len - num_of_conjunctions + k)
+            if decision_sum * 2 >= num_check_sums:
+                curr_mess[conj_number] += 1
+                curr_mess[conj_number] = curr_mess[conj_number] % 2
+            else:
+                curr_mess[conj_number] += 0
+        k += 1
+        decision_sum = 0
         for gamma_size in range(len(orthogonal_subspace_positions) + 1):
             for curr_orthogonal_subspace in combinations(orthogonal_subspace_positions, gamma_size):
+                j += 1
+                if (j % len_check_sum) == 0:
+                    decision_sum += (total_sum_for_seq_sums % 2)
+                    total_sum_for_seq_sums = 0
                 for t in range(len(conjunction) + 1):
                     for sub_comb in combinations(conjunction, t):
-                        i += 1
-                        check_sum = sum_of_powers_of_two(sub_comb, m) + sum_of_powers_of_two(curr_orthogonal_subspace, m)
-                        print(check_sum, conjunction, sub_comb, curr_orthogonal_subspace, orthogonal_subspace_positions)
-                        start.append(check_sum)
-                    check_sum = 0
-    return start
+                        check_sum = (sum_of_powers_of_two(sub_comb, m) +
+                                     sum_of_powers_of_two(curr_orthogonal_subspace, m))
+                        total_sum_for_seq_sums += word[check_sum]
+    return curr_mess
 
-print(decoder_step_for_fixed_curr_r(2, 4, []), len(decoder_step_for_fixed_curr_r(2, 4, [])))
-# вывод всех индексов суммирования
+
+rm = ReedMuller(2, 4)
+
+codeword = rm.encode([1]*5 + [1]*6)
+print(codeword, 'codeword')
+codeword[15] = 0
+print(codeword, 'noise added')
+print(decoder_step_for_fixed_curr_r(11, codeword,
+                                    [0]*11, 2, 4))
