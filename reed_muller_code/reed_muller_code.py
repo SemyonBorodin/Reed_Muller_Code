059ce87dfd799168a55a3adf93cccd79711f442f
Semyon Borodin <s.borodin@g.nsu.ru>
2024-12-05 07:40:52 +0700

    decode_v2 added

--- reed_muller_code/reed_muller_code.py
+++ reed_muller_code/reed_muller_code.py
@@ -87,9 +87,46 @@ class ReedMuller:
                                    num_check_sums + d*num_check_sums + t * int(2 ** self.m), 1)])
                     # print(t, 't'*10)
                     one_sum += temp_sum % 2
+                print(one_sum * 2, num_check_sums, len_check_sum, 'len_check_sum')
+                print(self.k - num_of_conjunctions + t, 'self.k - num_of_conjunctions + t')
+                if one_sum * 2 > num_check_sums:
+                    curr_mess[self.k - num_of_conjunctions + t] = 1
+                    one_sum = 0
+                else:
+                    curr_mess[self.k - num_of_conjunctions + t] = 0
+                    one_sum = 0
+                print(curr_mess, 'curr')
+        return curr_mess
+
+    def decode_v2(self, received):
+        curr_mess = [0] * self.k
+        for i in range(self.r, -1, -1):
+            list_of_index = decoder_step_for_fixed_curr_r(
+                self.k,
+                received,
+                curr_mess,
+                i,
+                self.r,
+                self.m, [])
+            print(len(list_of_index), len(list_of_index)/16, list_of_index)
+            num_of_conjunctions = int(sum([int(binom(self.m, b)) for b in range(i, self.r + 1, 1)]))
+            cur_num_conj = int(binom(self.m, i))
+            len_check_sum = 2 ** (self.m - i)
+            num_check_sums = 2 ** i
+            one_sum = 0
+            for t in range(cur_num_conj):
+                for d in range(num_check_sums):
+                    temp_sum = sum([received[int(list_of_index[w])]
+                                   for w in range(0 + d*len_check_sum + t * int(2 ** self.m),
+                                   len_check_sum + d*len_check_sum + t * int(2 ** self.m), 1)])
+                    print([received[int(list_of_index[w])] for w in range(0 + d*len_check_sum + t * int(2 ** self.m),
+                                   len_check_sum + d*len_check_sum + t * int(2 ** self.m), 1)])
+                    # print(t, 't'*10)
+                    temp_sum = temp_sum % 2
+                    one_sum += temp_sum
                 print(one_sum, len_check_sum, 'len_check_sum')
                 print(self.k - num_of_conjunctions + t, 'self.k - num_of_conjunctions + t')
-                if one_sum * 2 > len_check_sum:
+                if one_sum * 2 > num_check_sums:
                     curr_mess[self.k - num_of_conjunctions + t] = 1
                     one_sum = 0
                 else:
@@ -143,7 +180,7 @@ def decoder_step_for_fixed_curr_r(
 
 rm = ReedMuller(2, 4)
 # не корректно работает для малых r
-code_word = rm.encode([0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1])
+code_word = rm.encode([0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0])
 print(code_word)
-print(rm.decode(code_word))
+print(rm.decode_v2(code_word))
 print(code_word)
